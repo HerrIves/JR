eee0f0419ebe7ebce0ea6b222c923363d5074a93
HerrIves <spechtator@gmail.com>
2020-03-20 22:40:05 +0100

    add Hellojava

---
+++ HelloWorld.java
@@ -0,0 +1,14 @@
+package JR.001;
+
+/*
+первый тестовый файл для гитхаб
+*/
+
+import java.util.HashMap;
+import java.util.*;
+
+public class HelloWorld{
+  public static void main(String[] args){
+System.out.println("Hello World");    
+  }
+}
